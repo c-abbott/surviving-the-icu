f9a09cbe06ee41d6e605d8b0679c35234df1aa1c
c-abbott <callum.abbott@outlook.com>
2021-07-02 11:01:41 +0000

    :sparkles: Linear regression estimator

--- src/dowhy_helpers.py
+++ src/dowhy_helpers.py
@@ -4,13 +4,13 @@ import dowhy
 
 def naive_estimate(df, treatment, outcome, treatment_type=None):
     """
-        Input:
+    Input:
         df (pd.DataFrame) - Dataframe holding data in question.
         treatment (str) - Name of treatment variable. Must be binary.
         outcome (str) -  Name of outcome variable.
         treatment_type (str) - Type of treatment variable. Accepted types: bool, int.
 
-        Output:
+    Output:
         naive_est (float64) - Causal estimate with no confounders adjusted for.
     """
     # Variable type checking
@@ -24,3 +24,19 @@ def naive_estimate(df, treatment, outcome, treatment_type=None):
     # conduct naive causal estimate
     naive_est = np.mean(df_treatment[outcome]) - np.mean(df_control[outcome])
     return naive_est
+
+def linear_regression_estimator(model, estimand, ci=False, test_significance=False):
+    """
+    Input:
+        model (dowhy.CausalModel) - Causal graph.
+        estimand (dowhy.IdentifiedEstimand) - Causal estimand P(Y|do(T)) derived from model.
+        ci (bool) - Confidence interval flag.
+        test_significance - Get p-value of estimate flag.
+
+    Output:
+        lin_est (dowhy.CausalEstimate) - Linear regression causal estimate of identified estimand.
+    """
+    lin_est = model.estimate_effect(estimand, method_name="backdoor.linear_regression", 
+                                    control_value=0, treatment_value=1, confidence_intervals=ci, 
+                                    test_significance=test_significance)
+    return lin_est
